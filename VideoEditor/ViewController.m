b43be53ea6b553e76d5da1ac03c4db231d69ea40
Siam300 <89306483+Siam300@users.noreply.github.com>
2024-02-05 20:36:17 +0600

    Implemented video picker

--- VideoEditor/ViewController.m
+++ VideoEditor/ViewController.m
@@ -6,8 +6,14 @@
 //
 
 #import "ViewController.h"
+#import <MediaPlayer/MediaPlayer.h>
+#import <MobileCoreServices/MobileCoreServices.h>
+#import <AVKit/AVKit.h>
 
 @interface ViewController ()
+
+@property (strong, nonatomic) NSURL *videoURL;
+@property (strong, nonatomic) MPMoviePlayerController *videoController;
 
 @end
 
@@ -18,9 +24,51 @@
     // Do any additional setup after loading the view.
 }
 
+- (void)playVideo {
+    
+    // Dismiss the existing video
+    if (self.presentedViewController) {
+        [self.presentedViewController dismissViewControllerAnimated:YES completion:nil];
+    }
+    
+    if (@available(iOS 9.0, *)) {
+        AVPlayer *player = [AVPlayer playerWithURL:self.videoURL];
+        AVPlayerViewController *controller = [[AVPlayerViewController alloc] init];
+        
+        controller.player = player;
+        
+        [self presentViewController:controller animated:YES completion:^{
+            [player play];
+        }];
+    } else {
+        self.videoController = [[MPMoviePlayerController alloc] initWithContentURL:self.videoURL];
+        
+        [self presentViewController:self.videoController animated:YES completion:^{
+            [self.videoController play];
+        }];
+    }
+    
+    // Register for playback finish notification
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(videoPlayBackDidFinish:) name:MPMoviePlayerPlaybackDidFinishNotification object:nil];
+}
+
+- (void)documentPicker:(UIDocumentPickerViewController *)controller didPickDocumentsAtURLs:(NSArray<NSURL *> *)urls {
+    // Handle the picked documents (in this case, a video)
+    NSURL *pickedURL = [urls firstObject];
+    if (pickedURL) {
+        self.videoURL = pickedURL;
+        [self playVideo];
+    }
+}
+
+- (IBAction)pickVideo:(id)sender {
+    UIDocumentPickerViewController *documentPicker = [[UIDocumentPickerViewController alloc] initWithDocumentTypes:@[(NSString *)kUTTypeMovie] inMode:UIDocumentPickerModeImport];
+    documentPicker.delegate = self;
+    [self presentViewController:documentPicker animated:YES completion:nil];
+}
 
 - (IBAction)btnPlay:(id)sender {
-    NSString *path = [[NSBundle mainBundle] pathForResource:@"Video" ofType:@"mp4"];
+    NSString *path = [[NSBundle mainBundle] pathForResource:@"Video2" ofType:@"mp4"];
     NSURL *url = [NSURL fileURLWithPath:path];
     
     AVPlayer *player = [AVPlayer playerWithURL:url];
@@ -28,10 +76,9 @@
     
     controller.player = player;
     
-    //show video on fullscreen
-    controller.view.frame = self.view.bounds;
-    [[self view] addSubview: controller.view];
+    [self presentViewController:controller animated:YES completion:nil];
     
     [player play];
+
 }
 @end
